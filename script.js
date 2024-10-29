529ce520f850a79ae5e0a5ce16f31820684cc8f7
Nimira43 <n.rabone72@aol.com>
2024-10-29 11:42:23 +0000

    Options const declared and program functions layout set

---
+++ script.js
@@ -0,0 +1,32 @@
+const options = [
+  {id: 'option1',
+    text: 'Ferrari',
+    votes: 0
+  },
+  {id: 'option2',
+    text: 'McLaren',
+    votes: 0
+  },
+  {id: 'option3',
+    text: 'Mercedes',
+    votes: 0
+  },
+  {id: 'option4',
+    text: 'Red Bull',
+    votes: 0
+  },
+]
+
+
+const getTotalVotes = () => {
+
+}
+
+const submitVote = () => {
+}
+
+const displayResult = () => {
+
+}
+
+displayResult()
