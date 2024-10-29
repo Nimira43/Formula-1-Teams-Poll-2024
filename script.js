1722bc3b598ae13c72bd331e31131ae5198eb7a2
Nimira43 <n.rabone72@aol.com>
2024-10-29 12:01:14 +0000

    JavaScript written - checking for bugs....

--- script.js
+++ script.js
@@ -19,14 +19,41 @@ const options = [
 
 
 const getTotalVotes = () => {
-
+  return options.reduce((total, option) => total + option.votes, 0)
 }
 
 const submitVote = () => {
+  const selectedOption = document.querySelector('input[name="poll"]:checked' )
+  if (!selectedOption) {
+    alert('Please choose your team...')
+    return
+  }
+  const optionId = selectedOption.value
+  const selectedOptionObj = options.find((option) => option.id === optionId)
+  if (selectedOptionObj) {
+    selectedOptionObj.votes++
+    displayResult()
+  }
 }
 
 const displayResult = () => {
+  const result = document.getElementById('result')
+  result.innerHTML = ''
 
+  options.forEach((option) => {
+    const percentage = ((option.votes / getTotalVotes()) * 100).toFixed(2) || 0;
+    const barWidth = percentage > 0 ? percentage + '%' : '0%'
+    const optionResult = document.createElement('div')
+    optionResult.innerHTML = 
+      `
+      <span class = 'option-text'>${option.text}</span>
+      <div class = 'bar-container'>
+        <div class = 'bar' style='width: ${barWidth};'></div> 
+      </div>
+      <span class = 'percentage'>${percentage}%</span>
+      `
+    result.appendChild(optionResult)
+  })
 }
 
 displayResult()
